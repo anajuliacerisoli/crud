d0a465107abd3d617c6d4c1d1ebc947cbfc2ff42
Ana Julia Cerisoli <ana.cerisoli@escola.pr.gov.br>
2026-03-30 08:54:48 -0300

    atualização 30/03/26

--- index.php
+++ index.php
@@ -14,6 +14,11 @@ $resultado = mysqli_query($conexao, $sql);
         <form action="salvar.php" method="post">
             <label>Nome:</label>
             <input type ="text" name="nome" required>
+            <label>Preço:</label>
+            <input type ="number" name="preco" required>
+            <label>Categoria:</label>
+            <input type ="text" name="categoria" required>
+            <button type= "submit">Salvar</button>
         </form>
     </body>
 </html>
